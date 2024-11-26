7f27d5c0c15d3c644212642732509c825279f6b1
ruehan <node.js_@kakao.com>
2024-11-26 17:09:37 +0900

    fix: mapping 추가

--- src/mapping.ts
+++ src/mapping.ts
@@ -16,6 +16,8 @@ export const TAG_MAP: { [key: string]: string } = {
 	openlayers: "OpenLayers",
 	docker: "Docker",
 	openrouteservice: "OpenRouteService",
+	performance_optimization: "성능 최적화",
+	location_based_service: "위치기반서비스",
 };
 
 export const getCategoryInKorean = (englishCategory: string): string => {
